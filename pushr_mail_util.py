cc5aad8c12413fd4ce3ecd7a2e121d17b4f71f3f
SmBe19 <bsgame27@gmail.com>
2015-12-31 13:12:52 +0100

    add date to mail

--- pushr_mail_util.py
+++ pushr_mail_util.py
@@ -1,5 +1,9 @@
 from pushr_settings import PUSHR_SETTINGS
 import smtplib
+from email.mime.text import MIMEText
+import email.utils
+
+USE_MIME = False
 
 def send_mail(recipient_name, recipient_mail, blind_copy, subject, message):
     smtp = smtplib.SMTP(host=PUSHR_SETTINGS["smtp_server"], port=PUSHR_SETTINGS["smtp_port"])
@@ -8,7 +12,17 @@ def send_mail(recipient_name, recipient_mail, blind_copy, subject, message):
     sender = PUSHR_SETTINGS["sender_mail"]
     sender_full = PUSHR_SETTINGS["sender_name"] + " <" + PUSHR_SETTINGS["sender_mail"] + ">"
     recipient_full = recipient_name + " <" + recipient_mail + ">"
-    msg = "From: {0}\nTo: {1}\nSubject: {2}\n\n{3}".format(sender_full, recipient_full, subject, message)
+
+    if USE_MIME:
+        m = MIMEText(message.encode("utf-8"), _charset="utf-8")
+        m["To"] = recipient_full
+        m["From"] = sender_full
+        m["Subject"] = subject
+        m["Date"] = email.utils.formatdate()
+        msg = m.as_string()
+    else:
+        msg = "Date: {0}\nFrom: {1}\nTo: {2}\nSubject: {3}\n\n{4}".format(email.utils.formatdate(), sender_full, recipient_full, subject, message)
+
     smtp.sendmail(sender, recipient_mail, msg)
 
     for mail in blind_copy:
